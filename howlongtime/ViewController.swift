4627a1e4243df58922e4a10eaaca66bd194593f7
Adolfho Athyla <adolfo.athyla@gmail.com>
2018-05-31 22:12:39 -0300

    Initial Commit

---
+++ howlongtime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  howlongtime
+//
+//  Created by Adolfho Athyla on 31/05/2018.
+//  Copyright © 2018 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
