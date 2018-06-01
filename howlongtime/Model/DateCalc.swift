06e4981df1427f8383a3c93ef8e75c07dfbb416e
Adolfho Athyla <adolfo.athyla@gmail.com>
2018-05-31 22:41:17 -0300

    first commit

---
+++ howlongtime/Model/DateCalc.swift
@@ -0,0 +1,13 @@
+//
+//  DateCalc.swift
+//  howlongtime
+//
+//  Created by Adolfho Athyla on 31/05/2018.
+//  Copyright © 2018 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class DateCalc: NSObject {
+
+}
